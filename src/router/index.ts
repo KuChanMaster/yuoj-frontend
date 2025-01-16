a0ebbca5828c640b01c8381e42e736c941bf2a89
KuChanMaster <liushuaibzinga@outlook.com>
2025-01-16 10:16:53 +0800

    换行符问题

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,9 +1,9 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
-import { routes } from "@/router/routes";//将此文件中的路由配置抽象出来到routers.ts中
+import { routes } from "@/router/routes";
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
-  routes
+  routes,
 });
 
 export default router;
--- src/store/user.ts
+++ src/store/user.ts
@@ -5,12 +5,12 @@ import { UserControllerService } from "../../generated";
 
 export default {
   namespaced: true,
-  state: () => ({//存储状态信息，比如用户信息（包括以下的actions,mutations可以在vuex官网中查看）
+  state: () => ({
     loginUser: {
-      userName: "未登录"
-    }
+      userName: "未登录",
+    },
   }),
-  actions: {//执行异步操作，触发mutations
+  actions: {
     async getLoginUser({ commit, state }, payload) {
       // 从远程请求获取登录信息
       const res = await UserControllerService.getLoginUserUsingGet();
@@ -19,14 +19,14 @@ export default {
       } else {
         commit("updateUser", {
           ...state.loginUser,
-          userRole: ACCESS_ENUM.NOT_LOGIN
+          userRole: ACCESS_ENUM.NOT_LOGIN,
         });
       }
-    }
+    },
   },
-  mutations: {//定义啦更新变量的方法
+  mutations: {
     updateUser(state, payload) {
       state.loginUser = payload;
-    }
-  }
+    },
+  },
 } as StoreOptions<any>;
